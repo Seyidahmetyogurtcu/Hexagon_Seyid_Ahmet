d66f14a87f1ae6b11e7ee70e2ade4cd9898e11d0
Seyidahmetyogurtcu <yogurtcu00@hotmail.com>
2021-05-11 00:50:00 +0300

    HexGenerator.cs Updated
    - Hexagon gameobjects stored in array
    - Hexagons Select system added

--- HexagonSeyidAhmet/Assets/Scripts/HexGenerator.cs
+++ HexagonSeyidAhmet/Assets/Scripts/HexGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -13,17 +14,30 @@ public class HexGenerator : MonoBehaviour
     //public 1Vector3 vec3;
     readonly float iterasyon = 0.748f;
     public int gridSizeX = 8, gridSizeY = 9;
-
+    public float xx, yy;
 
     public Tilemap hexagonTileMap;
     Vector3 inputPosition;
-    Vector3 touchPositionWorld=Vector3.zero;
-    Vector3Int tileMapCordinate=Vector3Int.zero;
+    Vector3 touchPositionWorld = Vector3.zero;
+    Vector3Int tileMapCordinate = Vector3Int.zero;
     Vector3 touchedCellsCenterPositionWorld = Vector3.zero;
     Vector3 a = Vector3.zero;
+    GameObject[,] hexagons; //stores the data for all hex game objects 
+    float clicktime = 0;
+    float clickDelayTime = 0.5f;
+    List<Collider2D> sellectedHexColliders;
+    ContactFilter2D aaa;
+    Vector2 sellectedCorner1 = new Vector3(-0.5f, -0.25f); // hex 1.corner position from hex center
+    Vector2 sellectedCorner2 = new Vector3(0, -0.5f); // hex 2.corner position from hex center
+    Vector2 sellectedCorner3 = new Vector3(0.5f, -0.25f); // hex 3.corner position from hex center
+    Vector2 sellectedCorner4 = new Vector3(0.5f, 0.25f); // hex 4.corner position from hex center
+    Vector2 sellectedCorner5 = new Vector3(0, 0.5f); // hex 5.corner position from hex center
+    Vector2 sellectedCorner6 = new Vector3(-0.5f, 0.25f); // hex 6.corner position from hex center
+
+
     void Start()
     {
-       
+        hexagons = new GameObject[gridSizeX, gridSizeY];
         #region not used
         //// gridBrushBase =  component<GridBrushBase>();
         //// gridBrushBase.BoxFill(gridLayout, brushTarget, position);
@@ -46,11 +60,13 @@ public class HexGenerator : MonoBehaviour
         #endregion
 
         //this generates hexagons with tilemap
-        for (int x = 0; x <= gridSizeX; x++)
+        for (int x = 0; x < gridSizeX; x++)
         {
-            for (int y = 0; y <= gridSizeY; y++)
+            for (int y = 0; y < gridSizeY; y++)
             {
-                Instantiate(brushTarget, hexagonTileMap.CellToWorld(new Vector3Int(x, y, 0)), Quaternion.identity);
+                //hexagons[x][y] = brushTarget;
+                hexagons[x, y] = Instantiate(brushTarget, hexagonTileMap.CellToWorld(new Vector3Int(x, y, 0)), Quaternion.identity, this.transform);
+                hexagons[x, y].name = "Hexagon " + "X_" + x + " Y_" + y;
             }
         }
     }
@@ -64,21 +80,64 @@ public class HexGenerator : MonoBehaviour
 #endif
         touchPositionWorld = Camera.main.ScreenToWorldPoint(inputPosition);//then convert it to World position, 
         tileMapCordinate = hexagonTileMap.WorldToCell(touchPositionWorld);//then convert it to tile map cordinates,
-        touchedCellsCenterPositionWorld = hexagonTileMap.CellToWorld(tileMapCordinate);                   //then convert it to World point of the cell's center
+        touchedCellsCenterPositionWorld = hexagonTileMap.CellToWorld(tileMapCordinate);  //then convert it to World point of the cell's center
 
         /*Calculate thesellected point*/
         a = touchPositionWorld - touchedCellsCenterPositionWorld;
-       float theta= (Mathf.Atan2(a.y,a.x))* Mathf.Rad2Deg;
+        float theta = (Mathf.Atan2(a.y, a.x)) * Mathf.Rad2Deg;
         //
         for (int i = -3; i < 3; i++)
         {
             Debug.LogWarning(theta);
-            if (theta >= 60*i && theta < 60*(i+1))
+            if (theta >= 60 * i && theta < 60 * (i + 1))
             {
-                //(i+1).corner is sellected
-                Debug.Log("theta:"+ theta+"\n"+(i+4)+". corner sellected");
+                //(i+4).corner is sellected
+                switch (i + 4)
+                {
+                    case 4:
+                        Selected(sellectedCorner4);
+                        break;
+                    case 5:
+                        Selected(sellectedCorner5);
+                        break;
+                    case 6:
+                        Selected(sellectedCorner6);
+                        break;
+                    case 1:
+                        Selected(sellectedCorner1);
+                        break;
+                    case 2:
+                        Selected(sellectedCorner2);
+                        break;
+                    case 3:
+                        Selected(sellectedCorner3);
+                        break;
+
+                    default:
+                        break;
+                }
+                Debug.Log("theta:" + theta + "\n" + (i + 4) + ". corner sellected");
             }
+
         }
-       // Debug.Log(hexagonTileMap.CellToWorld(tileMapCordinate));
+        // Debug.Log(hexagonTileMap.CellToWorld(tileMapCordinate));
+    }
+
+    private void Selected(Vector2 corner)
+    {
+        if (Input.GetMouseButton(0))
+        {
+            /*racyast */
+            Collider2D[] bbb = Physics2D.OverlapCircleAll(corner + (Vector2)touchedCellsCenterPositionWorld, 0.2499f);
+            if (bbb.Length >= 3)       //gives limit for selection (if it is not border)
+            {
+                foreach (Collider2D bb in bbb)
+                {
+                    bb.transform.localScale = new Vector3(1.2f, 1.2f, 1);
+                }
+            }
+
+        }
     }
 }
+
